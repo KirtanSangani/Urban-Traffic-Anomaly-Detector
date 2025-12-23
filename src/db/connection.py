5e955da32e2c0e71d68c7e18d17633309dee9940
KirtanSangani <kirtansangani@gmail.com>
2025-12-22 19:59:20 -0500

    Committing the final simulated data and the connection between docker containers and simulated data. Starting to pull the data and create a proper dataframe from it

--- src/db/connection.py
+++ src/db/connection.py
@@ -2,15 +2,19 @@ import psycopg2
 import os
 import time
 
+#Database Configuration
 DB_HOST = os.getenv('DB_HOST', 'db')
 DB_PORT = os.getenv('DB_PORT', '5432')
 DB_NAME = os.getenv('DB_NAME', 'traffic_anomaly')
 DB_USER = os.getenv('DB_USER', 'traffic_user')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'traffic_pass')
 
+#Database Connection
 def get_db_connection():
+    #Retrying to connect to the database 3 times
     for i in range(3):
         try:
+            #Connecting to the database
             conn = psycopg2.connect(
                 host=DB_HOST,
                 port=DB_PORT,
@@ -21,17 +25,22 @@ def get_db_connection():
             print(f"Connected to the database: {DB_NAME}")
             break
         except psycopg2.Error as e:
+            #If the connection fails, retry 2 more times
             if i < 2:
                 print(f"Failed to connect to the database: {e}, retrying...")
                 time.sleep(1)
             else:
+                #If the connection fails, raise an error
                 print(f"Failed to connect to the database: {e}")
                 raise e
     
+    #If the connection fails, return None
     if conn is None:
         return
     
+    #Inserting data into the test table
     try:
+        #Creating a test table
         cursor = conn.cursor()
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS connection_test (
@@ -43,6 +52,7 @@ def get_db_connection():
         conn.commit()
         print("Test table created successfully.")
 
+        #Inserting data into the test table
         test_data = f"Data written at {time.strftime('%H:%M:%S')}"
         cursor.execute("INSERT INTO connection_test (test_value) VALUES (%s);", (test_data,))
         conn.commit()
@@ -53,11 +63,14 @@ def get_db_connection():
 
         print(f"Data read successfully: '{result[0]}'")
     except Exception as e:
+        #If the error occurs, rollback the transaction
         print(f"Error: {e}")
         conn.rollback()
     finally:
+        #Closing the connection
         if conn:
             conn.close()
 
+#Main function
 if __name__ == "__main__":
     get_db_connection()
